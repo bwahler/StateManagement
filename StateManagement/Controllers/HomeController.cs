c29c593b29a1134259bd8e8a00b92f4a763f06b3
bwahler <bmwahler@gmail.com>
2019-02-14 21:43:22 -0500

    Initial Commit

---
+++ StateManagement/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using StateManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<User> users = new List<User>();
+
+        public ActionResult Index()
+        {
+            ViewBag.CurrentUser = (User)Session["CurrentUser"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Products we can be proud of!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "We have Products!";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        public ActionResult UserDetails(User account)
+        {
+            if(Session["CurrentUser"] != null)
+            {
+                account = (User)Session["CurrentUser"];
+                ViewBag.CurrentUser = account;
+                return View();
+            }
+            else
+            {
+                if(ModelState.IsValid)
+                {
+                    users.Add(account);
+                    ViewBag.CurrentUser = account;
+                    Session["CurrentUser"] = account;
+                    return RedirectToAction("UserDetails");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Registration Failed. Please try again";
+                    return View("RegisterUser");
+                }
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            ViewBag.CurrentUser = (User)Session["CurrentUser"];
+            Session.Remove("CurrentUser");
+            return View();
+        }
+    }
+}
---
+++ StateManagement/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StateManagement.Models
+{
+    public class User
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = "Please enter a valid name.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = "Please enter a valid name.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{2,}$", ErrorMessage = "Please enter a valid username.")]
+        public string UserName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage = "Incorrect E-mail Format!")]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+
+        public User(string firstName, string lastName, string username, string email, int age, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            UserName = username;
+            Email = email;
+            Age = age;
+            Password = password;
+        }
+
+        public User() { }
+    }
+}
